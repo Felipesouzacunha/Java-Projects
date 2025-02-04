8011cca7a82ae4454172635ed9c5c88d8a87a0af
Felipe de Souza <131294693+Felipesouzacunha@users.noreply.github.com>
2025-02-04 03:14:35 -0300

    .

--- map/src/model/application/Program.java
+++ map/src/model/application/Program.java
@@ -3,37 +3,49 @@ package model.application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		Map<String, Integer> votes = new LinkedHashMap<>();
+
 		System.out.print("Enter file full path: ");
 		String path = sc.nextLine();
-		
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
-			Map<String, Double> c = new HashMap<String, Double>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
 			String line = br.readLine();
 			while (line != null) {
-				String [] fields = line.split(",");
-				c.put(fields[0], Double.parseDouble(fields[1]));
-				line = br.readLine();
 				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				int count = Integer.parseInt(fields[1]);
+
+				if (votes.containsKey(name)) {
+					int votesSoFar = votes.get(name);
+					votes.put(name, count + votesSoFar);
+				}
+				else {
+					votes.put(name, count);
+				}
+				
+				line = br.readLine();
 			}
-			for(String key : c.keySet()) {
-				System.out.println(key + ", " + c.get(key));
+			
+			for (String key : votes.keySet()) {
+				System.out.println(key + ": " + votes.get(key));
 			}
 			
 		} catch (IOException e) {
-			// TODO: handle exception
+			System.out.println("Error: " + e.getMessage());
 		}
+
+		sc.close();
 	}
 
 }
